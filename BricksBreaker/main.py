0094c3d6e31496f57a35438f70549692bd01b999
kakkarotssj <kanjeckcooldeva.005@gmail.com>
2017-05-30 23:40:09 +0530

    Removed few colors that are not needed

--- BricksBreaker/main.py
+++ BricksBreaker/main.py
@@ -14,9 +14,6 @@ SCREEN_HEIGHT = 731
 BALL_DIMENSIONS = [5, 5]
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
 FPS = 50
 PADDLE_WIDTH = 119
@@ -250,4 +247,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
